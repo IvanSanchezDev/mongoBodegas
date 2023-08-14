26924b8f7797ba38463e0e1d8ae515d8776a3efd
IvanSanchezDev <ivancampus2023@gmail.com>
2023-08-14 09:03:44 -0500

    feat: :sparkles: implementacion de rutas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import appBodegas from './routes/bodega.routes'
+import appProductos from './routes/producto.routes'
+import appInventario from './routes/inventario.routes'
 
 const app = express()
 app.use(express.json())
+
+app.use('/api/bodega', appBodegas)
+app.use('/api/producto', appProductos)
+app.use('/api/inventario', appInventario)
 
 app.use((req, res) => {
   res.send('404 not found')
 })
 
-const port = 1234
+const port = process.env.PORT ?? 1234
 
 app.listen(port, () => {
   console.log(`server listening on port http://localhost:${port}`)
